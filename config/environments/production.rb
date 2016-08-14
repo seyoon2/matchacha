917f53600f9532881505c1dafab4d41468c8887b
명세윤 <myeongseyoon@myeongseyun-ui-MacBook-Pro.local>
2016-08-14 17:57:48 +0900

    change host url of sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'matchacha.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'matchacha-test.herokuapp.com', :protocol => 'https'}
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
